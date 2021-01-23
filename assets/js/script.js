2f9625cfbec82a402d57c869a68a9a1a66065de6
Mark Pasricha <markp862@gmail.com>
2021-01-23 13:56:39 -0500

    working on api calls for second api

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,15 +13,21 @@ function parseBandNames(response){
 
 	return bandArr;
 }
-var bandName = encodeBandName("the doors")
-var queryURL = `https://tastedive.com/api/similar?q=${bandName}&k=400264-project1-2ZU40HSL`
+var bandName = encodeBandName("Iron Maiden")
+// var queryURL = `https://tastedive.com/api/similar?q=${bandName}&k=${APIKey}`
+var queryURL = 'https://api.deezer.com/search?q=eminem&output=jsonp'
+// var queryURL = 'https://api.deezer.com/track/119606&output=jsonp'
+// var queryURL = 'https://api.deezer.com/album/119606/tracks&output=jsonp'
+
+
 
 $.ajax({
 	url: queryURL, 
 	type: "GET",
 	dataType: 'jsonp',
 	success: function(response){
-		var bandNameArr = parseBandNames(response); 
-		console.log(bandNameArr);
+		// var bandNameArr = parseBandNames(response); 
+		// console.log(bandNameArr);
+		console.log(response)
 	}
 })
